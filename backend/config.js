d943d6d37d37490fe637363f086d0854820dd0ac
shahd Hesham <shahedhesham13@gmail.com>
2024-12-09 00:58:55 +0200

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,3 @@
 export const PORT = 3000;
 
-export const mongoDBURL =
-  'mongodb+srv://shahedhesham13:itsshahd@bookstore.zh1mn.mongodb.net/booksCollection?retryWrites=true&w=majority&appName=bookStore';
+export const mongoDBURL = '';
